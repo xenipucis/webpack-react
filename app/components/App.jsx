1bdaf47034baf46cfedcf099b8a2e12e19789f97
Tiberiu Adrian CARP <tcarp@localhost.localdomain>
2016-01-23 20:48:28 +0100

    Communicating Note State Changes to App.jsx

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -31,7 +31,7 @@ export default class App extends React.Component {
     return (
       <div>
        <button onClick={this.addNote}>+</button>
-       <Notes notes={notes} />
+       <Notes notes={notes} onEdit={this.editNote} />
       </div>
     );
   }
@@ -44,5 +44,16 @@ export default class App extends React.Component {
         task: 'New task'
       }])
     });
+  };
+
+  editNote = (id, task) => {
+    const notes = this.state.notes.map(note => {
+      if (note.id === id && task) {
+        note.task = task;
+      }
+
+      return note;
+    });
+    this.setState({notes});
   };
 }
