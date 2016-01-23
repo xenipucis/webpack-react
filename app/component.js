7543714ffc19212df13b7fac831aa16656b4082e
Tiberiu Adrian CARP <tcarp@localhost.localdomain>
2016-01-23 11:41:32 +0100

    Add react to project through babel configs

--- app/component.js
+++
@@ -1,7 +0,0 @@
-module.exports = function () {
-  var element = document.createElement('h1');
- 
-  element.innerHTML = 'Hello world. Thi:wqis the new modification';
-
-  return element;
-}
--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,12 @@ const common = {
   // Entry accepts a path or an object of entries.
   // The build chapter contains an example of the latter.
   entry: PATHS.app,
+  // Add resolve.extensions.
+  // '' is needed to allow imports without an extension.
+  // Note the .'s before extensions as it will fail to match without!!!
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
   output: {
     path: PATHS.build,
     filename: 'bundle.js'
@@ -25,6 +31,16 @@ const common = {
         loaders: ['style', 'css'],
         // Include accepts either a path or an array of paths.
         include: PATHS.app
+     },
+     // Set up jsx. this accepts js too thanks to RegExp
+     {
+       test: /\.jsx?$/,
+       // Enable caching for improved performance during development
+       // It uses default OS directory by default.
+       // If you need something more custom, pass a path to it.
+       // I.e., babel?cacheDirectory=<path>
+       loaders: ['babel?cacheDirectory'],
+       include: PATHS.app
      }
    ]
   }
