ae26619eb22af6cfd256f880d0d8f3c62cd772bb
Tiberiu Adrian CARP <tcarp@localhost.localdomain>
2016-01-23 19:25:11 +0100

    Starting with React

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react';
-import Note from './Note.jsx';
+import uuid from 'node-uuid';
 
 export default class App extends React.Component {
   render() {
-    return <Note />;
+    const notes = [
+      {
+        id: uuid.v4(),
+        task: 'Learn Webpack'
+      },
+      {
+        id: uuid.v4(),
+        task: 'Learn React'
+      },
+      {
+        id: uuid.v4(),
+        task: 'Do laundry'
+      }
+    ];
+
+    return (
+      <div>
+       <ul>{
+        notes.map(note =>
+          <li key={note.id}>{note.task}</li>)
+       }</ul>
+      </div>
+    );
   }
 }
