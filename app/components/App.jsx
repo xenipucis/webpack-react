fa63f920b441f3cb7a933c564b19cb3e681a0710
Tiberiu Adrian CARP <tcarp@localhost.localdomain>
2016-01-23 19:30:36 +0100

    Using state of component..

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,21 +2,30 @@ import React from 'react';
 import uuid from 'node-uuid';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      notes: [
+        {
+          id: uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Do laundry'
+        }
+      ]
+    };
+  }
+
   render() {
-    const notes = [
-      {
-        id: uuid.v4(),
-        task: 'Learn Webpack'
-      },
-      {
-        id: uuid.v4(),
-        task: 'Learn React'
-      },
-      {
-        id: uuid.v4(),
-        task: 'Do laundry'
-      }
-    ];
+
+    const notes = this.state.notes;
 
     return (
       <div>
